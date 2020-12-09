6b170e438e226598e1a11608e26e9b69d2a36779
daiphung1998 <phungthedai1998.hd@gmail.com>
2020-12-09 23:30:59 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ToDoList from './components/TodoList/Index';
-import './scss/style.css';
 
 class APP extends React.Component {
   render() {
     return (
       <div>
-        <ToDoList/>
       </div>
     )
   }
